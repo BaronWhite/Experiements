1427044be5cdf904a79ee7cc54b2a2464c5fbf61
Luis Balsa <luis.balsa@gentrack.com>
2020-02-19 19:28:12 +0000

    First hour

---
+++ Citywire coding test[1374]/App/App/CustomerCreditChecker.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App
+{
+    public interface ICustomerCreditCheck
+    {
+        void PerformCreditCheck(Customer customer, Company company);
+    }
+
+    class VeryImportantClientCreditCheck : ICustomerCreditCheck
+    {
+        public void PerformCreditCheck(Customer customer, Company company)
+        {
+            // Skip credit check
+            customer.HasCreditLimit = false;
+        }
+    }
+
+    class ImportantClientCreditCheck : ICustomerCreditCheck
+    {
+        public void PerformCreditCheck(Customer customer, Company company)
+        {
+            // Do credit check and double credit limit
+            customer.HasCreditLimit = true;
+            using (var customerCreditService = new CustomerCreditServiceClient())
+            {
+                var creditLimit = customerCreditService.GetCreditLimit(customer.Firstname, customer.Surname, customer.DateOfBirth);
+                creditLimit = creditLimit * 2;
+                customer.CreditLimit = creditLimit;
+            }
+        }
+    }
+
+    class NonImportantClientCreditCheck : ICustomerCreditCheck
+    {
+        public void PerformCreditCheck(Customer customer, Company company)
+        {
+            // Do credit check
+            customer.HasCreditLimit = true;
+            using (var customerCreditService = new CustomerCreditServiceClient())
+            {
+                var creditLimit = customerCreditService.GetCreditLimit(customer.Firstname, customer.Surname, customer.DateOfBirth);
+                customer.CreditLimit = creditLimit;
+            }
+        }
+    }
+
+    public class CreditCheckFactory
+    {
+        public ICustomerCreditCheck PerformCreditCheck(String company)
+        {
+            if (String.IsNullOrEmpty(company)) throw new Exception("No company name provided.");
+
+            
+            if (company == "VeryImportantClient")
+            {
+                return new VeryImportantClientCreditCheck();
+            }
+            else if (company == "ImportantClient")
+            {
+                return new ImportantClientCreditCheck();
+            }
+            else
+            {
+                return new NonImportantClientCreditCheck();
+            }
+        }
+    }
+}
---
+++ Citywire coding test[1374]/App/App/CustomerService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App
+{
+    public class CustomerService
+    {
+        public List<ValidationErrors>
+
+        public bool AddCustomer(string firname, string surname, string email, DateTime dateOfBirth, int companyId)
+        {
+
+
+
+            var companyRepository = new CompanyRepository();
+            var company = companyRepository.GetById(companyId);
+
+            var customer = new Customer
+                               {
+                                   Company = company,
+                                   DateOfBirth = dateOfBirth,
+                                   EmailAddress = email,
+                                   Firstname = firname,
+                                   Surname = surname
+                               };
+
+            var creditCheckFactory = new CreditCheckFactory();
+            var creditCheck = creditCheckFactory.PerformCreditCheck(company.Name);
+
+            creditCheck.PerformCreditCheck(customer, company);
+
+            if (customer.HasCreditLimit && customer.CreditLimit < 500)
+            {
+                return false;
+            }
+
+            CustomerDataAccess.AddCustomer(customer);
+
+            return true;
+        }
+
+        public bool ValidateName()
+        {
+                        if (string.IsNullOrEmpty(firname) || string.IsNullOrEmpty(surname))
+            {
+                return false;
+            }
+        }
+
+        public bool ValidateAge()
+        {
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
+
+            if (age < 21)
+            {
+                return false;
+            }
+        }
+
+        public bool ValidateEmail()
+        {
+                        if (!email.Contains("@") && !email.Contains("."))
+            {
+                return false;
+            }
+        }
+
+        public bool ValidateCredit()
+        {
+
+        }
+    }
+}
